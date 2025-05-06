453d319b669b561391c2d06407c72a485ab1fd3d
nvm1cz <nguyenvuminh05022005@gmail.com>
2025-05-06 12:04:00 +0700

    connected db

--- src/controller/DocumentController.java
+++ src/controller/DocumentController.java
@@ -1,8 +1,13 @@
 package controller;
 
 import java.net.URL;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import java.sql.Statement;
+
+import dao.Database;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,6 +26,88 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class DocumentController implements Initializable {
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private Statement statement;
+    private ResultSet result;
+
+    private double x = 0;
+    private double y = 0;
+    
+    public void login(){
+        
+        String sql = "SELECT * FROM students WHERE studentNumber = ? and password = ?";
+        
+        connect = Database.connectDB();
+        
+        try{
+            
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, studentNumber.getText());
+            prepare.setString(2, password.getText());
+            result = prepare.executeQuery();
+            
+            Alert alert;
+            
+            if(studentNumber.getText().isEmpty() || password.getText().isEmpty()){
+                
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Admin Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields.");
+                alert.showAndWait();
+            }else{
+                if(result.next()){
+                    
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Admin Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+                    
+//                    TO HIDE THE LOGIN FORM
+                    login_Btn.getScene().getWindow().hide();
+                    
+//                    FOR DASHBOARD
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                    
+                    Stage stage = new Stage();
+                    
+                    Scene scene = new Scene(root);
+                    
+                    root.setOnMousePressed((MouseEvent event) ->{
+                        
+                        x = event.getSceneX();
+                        y = event.getSceneY();
+                        
+                    });
+                    
+                    root.setOnMouseDragged((MouseEvent event) ->{
+                       
+                        stage.setX(event.getScreenX() - x);
+                        stage.setY(event.getScreenY() - y);
+                        
+                    });
+                    
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    
+                    stage.setScene(scene);
+                    stage.show();
+                    
+                }else{
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Admin Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username or Password.");
+                    alert.showAndWait();
+                }
+            }
+            
+        }catch(Exception e){e.printStackTrace();}
+        
+    }
+
 
     @FXML
     private TextField studentNumber;
--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 // 192.168.110.5
 public class DBConnection {
-    private static final String url = "jdbc:mysql://localhost:3306/hustbookstore";
+    private static final String url = "jdbc:mysql://localhost:3306/library_db";
     private static final String user = "root";
-    private static final String password = "vanthinh123";
+    private static final String password = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
