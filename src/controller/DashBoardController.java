dec1bba12476206910662cc08e5e68a48dcd6c01
nvm1cz <nguyenvuminh05022005@gmail.com>
2025-05-06 23:23:54 +0700

    design dashboard

---
+++ src/controller/DashBoardController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class DashBoardController implements Initializable {
+    // @override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+    }
+}
