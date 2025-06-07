0ca4b6dee1e3aae543f363350e1cb750cd82e9b3
nvm1cz <nguyenvuminh05022005@gmail.com>
2025-06-07 13:50:32 +0700

    Update Procedure_ReturnBook.sql

--- src/database/Procedure_ReturnBook.sql
+++ src/database/Procedure_ReturnBook.sql
@@ -4,7 +4,6 @@ CREATE PROCEDURE Procedure_ReturnBook(
     IN p_entry_id INT
 )
 BEGIN
-    -- No need to get book_id anymore since trigger will handle the update
     UPDATE BorrowEntry 
     SET ReturnDate = NOW() 
     WHERE EntryID = p_entry_id;
