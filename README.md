ebc2d4c0f905656e3e2dcb743bffca81e3b7355e
a <a@gmail.com>
2025-06-08 16:42:40 +0700

    clear

--- README.md
+++
@@ -1,2 +0,0 @@
-# Database Project
-MY LIBRARY
--- src/utils/PopUpNoti.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import javax.swing.JOptionPane;
-
-public class PopUpNoti {
-    public static void showAlert(String message) {
-        JOptionPane.showMessageDialog(null, message, "Alert", JOptionPane.ERROR_MESSAGE);
-    }
-}
